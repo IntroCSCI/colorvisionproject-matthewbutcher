af101e509be3e165e595d6885a05f66027d192f5
Matthew Butcher <>
2021-04-26 15:53:41 +0000

    i updated the whole code to version 0.2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # !!!Color finder !!!
+color finder finds the hex values in a the code checks if the code length is 6 or 3. from there the code will take the hex values and turn it into an intiger with that the code will then check if it falls in the correct values and if so will check if the previous pixel is to close to the current pixel in color and may pose a problem.
 
 ## Description
 
@@ -6,7 +7,10 @@
 
 ### v0.2 Updates
 
-*Coming soon*
+Updates
+addition of an improved file reader
+file now looks for the # before the hexvalues of color and then checks if the following string is 3 or 6 characters long
+created structures that are called pixels, this contains a
 
 ### v1.0 Updates
 
@@ -30,11 +34,12 @@ Here is an example of the program running:
 
 ```
 !!!Replace with an example of the program running with user interaction
+#A5B6C4
 
-color(20,20,30)
-Entered Red (20)
-Entered Green(20)
-Entered Blue(30)
+color(165,107,196)
+Entered Red (165)
+Entered Green(107)
+Entered Blue(196)
 !!!
 ```
 
@@ -44,7 +49,38 @@ Entered Blue(30)
 
 !!!
 i used int to represent the color levels of red green and blue because in a file those colors are represented by a number and the mixture of those numbers is how other colors are made.
-i made two variables for future development so that its possible to compare two colors later
+i made two variables for current pixel and future pixel i am using these to check if any of the color values are to close or might cause a problem
+there are a few data types in this code from srings to bools to int.
+these cand be see here
+string colorValues;
+  int redFirst;
+  int greenFirst;
+  int blueFirst;
+  string filename = "";
+  int bluesecond;
+  int redsecond;
+  int greensecond;
+  int firstColor;
+  int secondColor;
+  fstream reader;
+  char repeat;
+  string line = "";
+  size_t point;
+  string color;
+  string input;
+  int counter;
+  string lastColor;
+  bool keepGoing;
+  pixel singlePixel;
+  i made a new typle called pixel that is connectede to the struct called pixel. this lets me stor a certain amount of data in a orded area. 
+  struct pixel
+{
+  string red;
+  string green;
+  string blue;
+  // tone;
+};
+in pixel the red green and blue values are stored and each pixel has its own values.
 
 
 !!!
@@ -62,12 +98,16 @@ i used a decisions by asking the user to input a file.
 while not the biggest decision its still an important one.
 future decisions might inclue wanting to look out for certain color mixes or even change colors.
 
+multiple decisions are being made in this file from if the following string is correct and should continue on the code to if the code should use the vareint made for a length of  six or if the code should use the three length version. 
+
+
+
 !!!
 
 ### Iteration
 
 !!!
-for my program i am having having it run thought each pixel and having it then display the whole rgb value and a broken up value that is easier to understand. this will keep going for every line until it reaches the end of the image and then quites.
+for my program i am having having it run thought each line of code find the # symbol and having it then display the whole rgb value which would be a hexidecimal. from there the string is broken up value that is easier to understand and check the values against the previous color value. this will keep going for every line until it reaches the end of the image and then quites.
 !!!
 
 ### File Input and Output
@@ -76,17 +116,59 @@ for my program i am having having it run thought each pixel and having it then d
 
 i am asking my program to use the inputed file and from there go though each pixel in the array and find the rgb values from there my program will display the total color value and break up that value into red green blue to be more understanable to those who dont know  programing.
 
+ getline(cin, filename);
+ this gets the file name that is entered
+ reader.open(filename, ios::in | ios::out);
+this opens the file based on the entered file name
+ if(reader.is_open())
+ this if says that follow the code if the reader is open
+    {
+     while (!reader.eof()) 
+     this says while the file is not at the end of the file follow this code
+     {
+       getline(reader, line);
+       this line reades the file and uses the string line
+       point = line.find("#");
+       this code uses point and says that point is the line when it looks though a file and finds a # and the characters after that.
+
 
 
 !!!
 
 ### Arrays/Vectors
 
-*Coming in version 0.2*
+creates an array for the color values. each color found in the file is added to a vector. there are vectors for each red blue and green values of each pixel.
+example
+redNumbers.push_back(line.substr (0,1));
+this takes the substring of the line at points 0 and points 1 and adds it to the vector that contains all the red values. this is to keep a record of all the red values.
+there are color vectors for both green and blue
 
 ### Functions
 
-*Coming in version 0.2*
+multiple functions are coming in this update, one of the main function is the color check function. the color check function checks if a color is 5 to 10 rgb int off from the color before it and if it is it gives a warning about potential color blindness issues.
+example 
+bool isHexColor( const string & values )
+{
+  
+  if( values.length() == 3 || values.length() == 6 )
+  {
+    for(int i=0; i < values.length(); i++)
+    {
+      if( !(values[i] >= '0' && values[i] <='9') &&
+          !(values[i] >= 'a' && values[i] <= 'f') &&
+          !(values[i] >= 'A' && values[i] <= 'F') )
+          {
+            return false;
+          }
+    }
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+what this function is doing is it is checking the length of the string and seeing if it does indeed match up to the values a pixel color would be. from there it checking the characters to see if the characters are 0 though 9 a though f and A though F) if not then it will stop and return false but if it does contain those characters all the way though then the function returns a true to the main program and the main program will proceed.
 
 ### Classes
 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,34 @@
 //Author: Matthew Butcher
 #include <iostream>
 #include <fstream>
+#include <vector>
+using namespace std;
+//#include <struct>
 //#include "libraries/bitmap.h"
-
+void colorCheck(string );
+bool isHexColor( const string & );
+bool colorVerify(string ,string, string );
+bool colorDisplay(int, int, int);
+void colorDominence(string, string, string);
+void colorTest(int, int, int, int, int, int, string, string);
+//void colorValueHex(string );
 //  switch to SVG
 
-using namespace std;
+struct pixel
+{
+  string red;
+  string green;
+  string blue;
+  // tone;
+};
+
 
 int main()
 {
-  int redfirst;
-  int greenfirst;
-  int bluefirst;
+  string colorValues;
+  int redFirst;
+  int greenFirst;
+  int blueFirst;
   string filename = "";
   int bluesecond;
   int redsecond;
@@ -25,90 +42,265 @@ int main()
   string color;
   string input;
   int counter;
- //color = (0, 0, 0, 0);
+  string lastColor;
+  bool keepGoing;
+  pixel singlePixel;
+
+  vector <string> redNumbers;
+  vector <string> blueNumbers;
+  vector <string> greenNumbers;
   
+ //color = (0, 0, 0, 0);
 //Palette Analyzer bitmap
   //do{
     //rgb
   cout << "Please enter the File to open:\n";
   //cin >> filename;
+  //6 string 
+  //01 red 23 green 45 blue
+  //sub string
+  //indexing i character at at color[0] color[1]
+  //string red = color[0] + color[1];
+  // deal with one or the other can do that
+  // need both by the endl
+  //substring position want to start
+  //
+  //use .length or . size
   
     getline(cin, filename);
     
     //getline(cin,file);
    // reader.open(file);
    reader.open(filename, ios::in | ios::out);
-  
+  //line reader still works fine
     if(reader.is_open())
     {
      while (!reader.eof()) 
      {
        getline(reader, line);
-       point = line.find("(");
-       //color = ;
-       if(line[point])
-       {
-       cout<<'(';
+       point = line.find("#");
+       
+       //if(line[point])
+       //{
+       //cout<<'(';
        input.size();
        if(point != string::npos)
-{
-  counter = point+1;
-    //cout<< counter;
-  for(int counter = 0; counter < input.size(); counter++){
+       {
+        counter = point+1;
+        //counter
+        //cout<< counter;
+         for(int counter = 0; counter < input.size(); counter++)
+         {
+           //if (input[counter] >= '0' && input[counter] <= '9')
+           keepGoing = isHexColor(input[counter]);
+           //keepGoing = isHexCounter(input[counter]);
 
-    if (input[counter] >= '0' && input[counter] <= '9')
-    {
-      
-    cout<< input[counter];
-
-
-    }
-  }
-
-
-     
-
-
-
-    do
-     {
-       cin>>redfirst;
-       cout<<"red value "<<redfirst;
-
-     }while( redfirst <0 || redfirst > 255);
-
-    do
-     {
-
-       cin>>greenfirst;
-       cout<<"green value "<<greenfirst;
-
-    }while( greenfirst < 0 || greenfirst > 255);
-
-    do
-    {
-
-       cin>>bluefirst;
-       cout<<"blue value "<<bluefirst;
-     }while( bluefirst <0 || bluefirst > 255);
-
-
+           if(keepGoing = true && input.lenght = 6);
+          {
+          cout<< input[counter];
+          singlePixel.red = line.substr (0,1);
+          singlePixel.blue = line.substr (2,3);
+          singlePixel.green = line.substr (4,5);
+          //std::stringstream ss
+          redFirst = stoi(singlePixel.red, 0, 16);
+          blueFirst = stoi(singlePixel.blue, 0, 16);
+          greenFirst = stoi(singlePixel.green, 0, 16);
+          //if( .length() == 3) || values.length() == 6 )
+          //if( .length() == 3) || values.length() == 6 )
+          bool colorDisplay(redFirst, blueFirst, greenFirst);
+          redNumbers.push_back(line.substr (0,1));
+          blueNumbers.push_back(line.substr (2,3));
+          greenNumbers.push_back(line.substr (4,5));
+          if(counter < 0);
+          {
+            //
+            void colorTest(redFirst, blueFirst, greenFirst, redsecond,bluesecond, greensecond,line, lastColor);
+          }
+          else if(keepGoing = true && input.length = 3);
+          {
+          cout<< input[counter];
+          singlePixel.red = line.substr (0);
+          singlePixel.blue = line.substr (2);
+          singlePixel.green = line.substr (3);
+          //std::stringstream ss
+          redFirst = stoi(singlePixel.red, 0, 16);
+          blueFirst = stoi(singlePixel.blue, 0, 16);
+          greenFirst = stoi(singlePixel.green, 0, 16);
+          //if( .length() == 3) || values.length() == 6 )
+          //if( .length() == 3) || values.length() == 6 )
+          bool colorDisplay(redFirst, blueFirst, greenFirst);
+          redNumbers.push_back(line.substr (0,));
+          blueNumbers.push_back(line.substr (1));
+          greenNumbers.push_back(line.substr (2));
+          if(counter < 0);
+          {
+            //
+            void colorTest(redFirst, blueFirst, greenFirst, redsecond,bluesecond, greensecond,line, lastColor);
+          }
+          redsecond = redFirst;
+          bluesecond = blueFirst;
+          greensecond = greenFirst;
+          lastColor = line;
+          //redNumbers.push_back(line.substr (0,1));
+          //blueNumbers.push_back(line.substr (2,3));
+          //greenNumbers.push_back(line.substr (4,5));
+          //for(int counter = 0; counter < input.size(); counter++){
+          //if (input[counter] >= '0' && input[counter] <= '9')
+           //{
+           //---colorValues= input[counter];
+           //}
+          }
+        }
+  //fstream
+  //getline
+  //within string find where rgb
+  //.find funciton
+  //read 
+  //read then coma
+  // read hexadecima 
+  // first two first red second 2 green last 2 blue
+  //void colortest()
   // idea is to repeat this step for rgb second and have the program turn the old colorfirst to color second
-  // bluesecond = bluefirst
-        
+  // bluesecond = bluefirst    
        }
      }
-    }
     //if ((firstColor=colorconflict1 && secondColor = colorconflict2)||(firstColor=colorconflict2 && secondColor = colorconflict1));
     //{
       //cout="error, potential color blindness problem at "<<endl;
     //}
-
-
-
   //}while
     }
+    //secondPixel = input;
   reader.close();
   return 0;
 }
 
+bool isHexColor( const string & values )
+{
+  
+  if( values.length() == 3 || values.length() == 6 )
+  {
+    for(int i=0; i < values.length(); i++)
+    {
+      if( !(values[i] >= '0' && values[i] <='9') &&
+          !(values[i] >= 'a' && values[i] <= 'f') &&
+          !(values[i] >= 'A' && values[i] <= 'F') )
+          {
+            return false;
+          }
+    }
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//reads pixel and output color variable
+
+//is colors withing parameters
+bool colorDisplay( int redf, int greenf, int bluef)
+{
+  bool redtruth;
+  bool greentruth;
+  bool bluetruth;
+
+    do
+     {
+       //cin>>redf;
+       cout<<"red value "<<redf;
+       redtruth = true;
+     }while( redf < 0 || redf > 255);
+
+    do
+     {
+       //cin>>greenf;
+       cout<<"green value "<<greenf;
+       greentruth = true;
+
+     }while( greenf < 0 || greenf > 255);
+
+    do
+    {
+
+       //cin>>bluef;
+       cout<<"blue value "<<bluef;
+       bluetruth = true;
+       
+     }while( bluef < 0 || bluef > 255);
+
+    if (redtruth == true && bluetruth == true && greentruth == true);
+    {
+    return true;
+    }
+    else;
+    {
+      return false;
+    }
+    
+}
+
+
+void colorTest(int redP1, int blueP1, int greenP1, int redP2, int blueP2, int greenP2, string color1, string color2);
+{
+bool redTruth;
+bool blueTruth;
+bool greenTruth;
+
+
+if (redP1 - redP2 < -5 || 5) && (redP1 - redP2 < 10 || -10);
+{
+  //
+  cout<<"potential issue between red colors values"<<redP1<<"and"<<redP2<<endl;
+  redTruth = false;
+
+}
+if (blueP1 - blueP2 < -5 || 5) && (blueP1 - blueP2 < 10 || -10);
+{
+  //
+  cout<<"potential issue between red colors values"<<blueP1<<"and"<<blueP2<<endl;
+  blueTruth = false;
+}
+if (greenP1 - greenP2 < -5 || 5) && (greenP1 - greenP2 < 10 || -10);
+{
+  //
+  cout<<"potential issue between red colors values"<<greenP1<<"and"<<greenP2<<endl;
+  greenTruth = false;
+}
+
+if( redTruth = false || blueTruth = false || greenTruth = false);
+{
+  cout<<"potential issue between colors"<<color1<<"and"<<color2<<endl;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void colorTest(string redC, string blueC, string greenC)
+//{
+
+
+
+//}
+
+
+
+
+// if pixel 
+// v
+//check if colors are to close to each other 
+//void colorCheck(string )
+//{
+
+//}
